3c8bdc179d6dc62329fd0a71000c8c645cada588
chandu0077 <chandu.dsc77@gmail.com>
2023-05-18 16:34:49 +0530

    Intial Commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById("quote-conatiner");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+// Show new Quote
+function newQuote() {
+  loading();
+  // Pick a randomn quote froma apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //   Check if the author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //   Check the quote length to determine the styling
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  //   Set Quote,Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
